56c645aed22e1efcb5d6c02f0091832c6682108a
Keust Silvano <keustps@gmail.com>
2020-02-05 22:31:05 -0300

    corrigindo nome do field no migrations.

--- migrations/1580951384460-initial.js
+++ migrations/1580951384460-initial.js
@@ -4,14 +4,14 @@ const User = require('../src/models/user');
  * Make any changes you need to make to the database here
  */
 async function up () {
-  await User.insertMany({ userName: 'Alice', password: 'alice' }, { userName: 'Bob', password: 'bob' });
+  await User.insertMany({ username: 'Alice', password: 'alice' }, { username: 'Bob', password: 'bob' });
 }
 
 /**
  * Make any changes that UNDO the up function side effects here (if possible)
  */
 async function down () {
-    await User.deleteMany({ userName: 'Alice' }, { userName: 'Bob' });
+    await User.deleteMany({ username: 'Alice' }, { username: 'Bob' });
 }
 
 module.exports = { up, down };
