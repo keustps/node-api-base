7fec1a4ddf949a1944781f28326cce6af4ec5e9e
Keust Silvano <keustps@pbh.gov.br>
2020-02-19 15:25:57 -0300

    Alterando o transport layer do winston para o ambiente de teste.

--- src/utils/logger/index.js
+++ src/utils/logger/index.js
@@ -8,11 +8,24 @@ let logger;
 if (config.env == 'test') {
     logger = new winston.createLogger({
         transports : [
-            new winston.transports.File({ filename: 'test/tests.log' })
+            new winston.transports.File({
+                filename: 'test/tests.log',
+                format: winston.format.combine(
+                    winston.format.timestamp({
+                        format: 'DD-MM-YYYY hh:mm:ss A ZZ'
+                    }),
+                    winston.format.json()
+                ),
+                handleExceptions: true
+            })
         ]
     });
 }else if (config.env == 'local' || config.env == 'development') {
-    logger = console;
+    logger = new winston.createLogger({
+        transports : [
+            new winston.transports.Console({ timestamp: true })
+        ]
+    });
 } else {
     const papertrailTransport = new winston.transports.Papertrail({
         host: config.logger.host,
